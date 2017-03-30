41aa44897fbd765b507efd5bf560250469f68e9d
ecirmoa <ecirmoa@gmail.com>
2017-03-31 02:24:15 +0900

    shrink

--- src/sdl2_cocoa.m
+++ src/sdl2_cocoa.m
@@ -17,28 +17,25 @@ static mrb_value mrb_sdl2_cocoa_change_transparency(mrb_state *mrb, mrb_value se
   SDL_Window *window = mrb_sdl2_video_window_get_ptr(mrb, self);
   SDL_SysWMinfo info;
   if( SDL_GetWindowWMInfo(window, &info) == false ) {
-    printf("SDL_GetWindowWMInfo from %d FAILED.\n", window);
+    // printf("SDL_GetWindowWMInfo from %d FAILED.\n", window);
     return mrb_nil_value();
   }
   NSWindow *nswindow = info.info.cocoa.window;
 
   @autoreleasepool {
-    NSLog(@"nswindow is %@\n",nswindow);
-    NSLog(@"contentView is %@\n",[nswindow contentView]);
+    // NSLog(@"nswindow is %@\n",nswindow);
+    // NSLog(@"contentView is %@\n",[nswindow contentView]);
     [nswindow setOpaque:NO];
     [nswindow setBackgroundColor:[NSColor clearColor]];
+
+    NSOpenGLContext* context = [NSOpenGLContext currentContext];
+    // NSLog(@"NSOpenGLContext %@\n", context);
+    GLint opaque = 0;
+    [context setValues:&opaque forParameter:NSOpenGLCPSurfaceOpacity];
+
     return self;
   }
 }
-
-static mrb_value mrb_sdl2_cocoa_change_transparency_gl_context(mrb_state *mrb, mrb_value self)
-{ @autoreleasepool {
-  NSOpenGLContext* context = [NSOpenGLContext currentContext];
-  NSLog(@"NSOpenGLContext %@\n", context);
-  GLint opaque = 0;
-  [context setValues:&opaque forParameter:NSOpenGLCPSurfaceOpacity];
-  return mrb_nil_value();
-} }
 
 void mrb_mruby_sdl2_cocoa_gem_init(mrb_state *mrb) {
   int arena_size;
@@ -50,8 +47,7 @@ void mrb_mruby_sdl2_cocoa_gem_init(mrb_state *mrb) {
   class_Window = mrb_class_get_under(mrb, mod_Video, "Window");
 
   mrb_define_method(mrb, class_Window, "change_transparency", mrb_sdl2_cocoa_change_transparency, MRB_ARGS_NONE() );
-  mrb_define_method(mrb, class_Window, "change_transparency_gl_context", mrb_sdl2_cocoa_change_transparency_gl_context, MRB_ARGS_NONE() );
-
+  
   arena_size = mrb_gc_arena_save(mrb);
 
   mrb_gc_arena_restore(mrb, arena_size);
